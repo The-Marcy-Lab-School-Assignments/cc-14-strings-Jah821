21f23ca65fa976b53d0056286708014bb1400c49
Jah821 <91166820+Jah821@users.noreply.github.com>
2021-11-10 15:44:16 +0000

    finished

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //write your code here
+function stripUpperCase(str) {
+    let ok = ""
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== str[i].toUpperCase()) {
+            ok += str[i]
+        }
+
+    }
+    return ok
+};
